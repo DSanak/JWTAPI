d8412f2578efd902a23398e7c810feecfc674681
Dominik Sanak <dominik.sanak159@gmail.com>
2022-10-25 20:07:50 +0200

    Add IUser.cs

---
+++ Interface/IUser.cs
@@ -0,0 +1,14 @@
+﻿using JWTAPI.Models;
+
+namespace JWTAPI.Interface
+{
+    public interface IUser
+    {
+        public List<UserInfo> GetUserDetails();
+        public UserInfo GetUserDetails(int id);
+        public void AddUser(UserInfo User);
+        public void UpdateUser(UserInfo User);
+        public UserInfo DeleteUser(int id);
+        public bool CheckUser(int id);
+    }
+}
