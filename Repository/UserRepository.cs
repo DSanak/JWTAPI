3a8ebe1762399536f1b9fa883f5ec6d7cd144efb
Dominik Sanak <dominik.sanak159@gmail.com>
2022-10-25 20:08:10 +0200

    Add User Repository

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,102 @@
+﻿using JWTAPI.Interface;
+using JWTAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTAPI.Repository
+{
+    public class UserRepository : IUser
+    {
+        readonly DatabaseContext _dbContext = new();
+
+        public UserRepository(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<UserInfo> GetUserDetails()
+        {
+            try
+            {
+                return _dbContext.UserInfos.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public UserInfo GetUserDetails(int id)
+        {
+            try
+            {
+                UserInfo? users = _dbContext.UserInfos.Find(id);
+                if (users != null)
+                {
+                    return users;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void AddUser(UserInfo users)
+        {
+            try
+            {
+                _dbContext.UserInfos.Add(users);
+                _dbContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void UpdateUser(UserInfo users)
+        {
+            try
+            {
+                _dbContext.Entry(users).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public UserInfo DeleteUser(int id)
+        {
+            try
+            {
+                UserInfo? users = _dbContext.UserInfos.Find(id);
+
+                if (users != null)
+                {
+                    _dbContext.UserInfos.Remove(users);
+                    _dbContext.SaveChanges();
+                    return users;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public bool CheckUser(int id)
+        {
+            return _dbContext.UserInfos.Any(e => e.UserId == id);
+        }
+    }
+}
