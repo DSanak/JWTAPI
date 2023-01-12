27489d6ced84acdba0ecc0aab657cebeed6a0c73
Dominik Sanak <dominik.sanak159@gmail.com>
2023-01-12 19:47:07 +0100

    Revert "Style for user login"
    This reverts commit cedf687686c1e36b5a85f3beb0250fcbeaa346f4.

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -156,16 +156,6 @@ namespace JWTAPI.Controllers
 
         }
 
-        [Route("stylelogowanie.css")]
-        [HttpGet]
-        public ActionResult LogowanieCss()
-        {
-            var fileContent = System.IO.File.ReadAllText("Pages/stylelogowanie.css");
-            return base.Content(fileContent, "text/css");
-
-
-        }
-
 
 
         private bool EmployeeExists(int id)
